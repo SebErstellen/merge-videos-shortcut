09bda8e03eb0e3895e50ac8801e991f06556ad4b
Chien Chin <flounderqin@outlook.com>
2023-01-16 22:23:41 +0800

    Create runshellscript.sh

---
+++ runshellscript.sh
@@ -0,0 +1,22 @@
+#! /bin/zsh
+
+cd $(dirname $1)
+
+touch list.txt
+
+for arg in "$@"; do
+  echo "file '$(basename "$arg")'" >> list.txt
+done
+
+file_name_without_extension=$(echo "$1" | cut -f 1 -d '.')
+merged_file_name="$file_name_without_extension"_merged.$(echo "$1" | cut -f 2 -d '.')
+
+/opt/homebrew/bin/ffmpeg -f concat -safe 0 -i list.txt -c copy $merged_file_name
+
+rm list.txt
+
+for file in "$@"
+do
+rm "$file"
+echo "File $file deleted successfully."
+done
